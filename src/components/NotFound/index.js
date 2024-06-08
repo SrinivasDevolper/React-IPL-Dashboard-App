99c2ea8a4ed6b816015ae55393b583ac573e51dd
srinivasDevolper <kingsrinivassri123@gmail.com>
2024-06-08 18:18:45 +0000

    first commit

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const NotFound = () => {
+  return (
+    <div className="bg-container-page">
+      <div className="container-page">
+        <h1 className="logo-heading">Page Not Found!!</h1>
+      </div>
+    </div>
+  )
+}
+
+export default NotFound
